3aee6ed4b3f98838fb96bc808883cf45aa3a28be
Fran Jaquet <franciscojaquet29@gmail.com>
2023-07-30 15:45:07 -0300

    probando tailwind

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,12 @@
 function Header () {
     return(
-        <header id="header">
-            <h1>hola mundo!</h1>
+        <header id="header" className="header flex justify-between py-4 px-2">
+            <h1>Delicias Caseras</h1>
+            <nav className="">
+                <a className="px-2" href="#">link</a>
+                <a className="px-2" href="#">link</a>
+                <a className="px-2" href="#">link</a>
+            </nav>
         </header> 
     )
 }
