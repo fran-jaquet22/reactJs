7a1786e8c1d78b1b4abc3379a28cee78a1853b4f
Fran Jaquet <franciscojaquet29@gmail.com>
2023-07-31 12:38:10 -0300

    avance en los componentes

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,16 @@
-function Main(){
+function Main(props){
+
+    let contador = 0
+
+    const handleClick = () => {
+        contador ++
+        console.log(contador)
+    }
+
     return(
-        <main>
-            <h2>Home</h2>
-            <p>Bienvenido </p>
+        <main className="p-2 grow">
+            <p>contador : {contador}</p>
+            <button onClick={handleClick}>click</button>
         </main>
     )
 }
