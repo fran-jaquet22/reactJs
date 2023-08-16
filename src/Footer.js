632efe37a676f87f161cf967c468dae724b899f9
Fran Jaquet <franciscojaquet29@gmail.com>
2023-08-16 19:28:15 -0300

    arreglos en el maquetado

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import NavLinks from "./components/NavLinks"
 
 function Footer() {
   return (
-    <footer className="px-5 py-6 bg-pink-800">
+    <footer className="px-5 py-6 bg-pink-700">
       <h3 className="mb-3">&copy; Todos los derechos reservados </h3> 
       <NavLinks/>
     </footer>
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import NavLinks from "./components/NavLinks"
 
 function Header () {
     return(
-        <header id="header" className="header flex justify-between py-7 px-5 bg-pink-800">
+        <header id="header" className="header flex justify-between py-7 px-5 bg-pink-700">
             <h1 className="">Delicias Caseras</h1>
             <NavLinks />
             <CartWidget/>
