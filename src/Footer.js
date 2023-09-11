d424efb173d0ae8366ff4ac9bb72cf4d57f335d6
Fran Jaquet <franciscojaquet29@gmail.com>
2023-09-11 19:02:09 -0300

    arreglando el maquetado

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,8 @@
 function Footer() {
   return (
-    <footer className="px-5 py-6 bg-pink-700 text-lg font-semibold text-center">
+    <footer className="px-5 py-6 bg-pink-500 text-lg font-semibold text-center">
       <h3 className="mb-3">&copy; Todos los derechos reservados </h3> 
+      <h4><a href="https://github.com/fran-jaquet22" target="blank_">GitHub</a></h4>
     </footer>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import NavLinks from "./components/NavLinks"
 
 function Header () {
     return(
-        <header id="header" className="header flex justify-between py-7 px-5 bg-pink-700">
+        <header id="header" className="header flex justify-between py-7 px-5 bg-pink-500">
             <Link to="/">
                 <img src="./logoDeli.jpeg" className="h-40" alt="" />
             </Link>
